6c11af9b3d8509ccc0d00b33caf74fcf595f7104
JoelRV <joelrrv@hotmail.com>
2022-08-30 14:04:19 +0200

    added pop method

--- section_19/singly_linked_list_structure.js
+++ section_19/singly_linked_list_structure.js
@@ -31,10 +31,44 @@ class SinglyLinkedList {
         this.length++;
         return this; // returning the new list
     }
+
+    // If there are no nodes in the list, return undefined
+    // Loop through the list until you reach the tail
+    // Set the next property of the 2nd to last node to be null
+    // Set the tail to be the 2nd to last node
+    // Decrement the length of the list by 1
+    // Return the value of the node removed
+    pop() {
+        if (!this.head) return undefined;
+
+        let current = this.head;
+        let previous = current;
+        while (current.next) {
+            previous = current;
+            current = current.next;
+        }
+        this.tail = previous;
+        this.tail.next = null;
+
+        this.length--;
+        // behaviour when list is empty
+        if (this.length === 0) {
+            this.head = null;
+            this.tail = null;
+        }
+        return current;
+    }
+
+    shift() {}
 }
 
 let list = new SinglyLinkedList();
 list.push("hello");
 list.push("there");
 list.push("friend");
+console.log(list);
+list.pop();
+list.pop();
+list.pop();
+list.pop();
 console.log(list);
