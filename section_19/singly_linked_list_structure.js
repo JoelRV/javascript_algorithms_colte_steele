d777d39f84f4a04ed1641af82da439a1f7ae11dd
JoelRV <joelrrv@hotmail.com>
2022-08-30 14:40:51 +0200

    implemented set method

--- section_19/singly_linked_list_structure.js
+++ section_19/singly_linked_list_structure.js
@@ -109,6 +109,17 @@ class SinglyLinkedList {
         }
         return currentNode;
     }
+
+    // This function should accept a value and an index
+    // Use your get function to find the specific node.
+    // If the node is not found, return false
+    // If the node is found, set the value of that node to be the value passed to the function and return true
+    set(val, index) {
+        let target = this.get(index);
+        if (!target) return false;
+        target.val = val;
+        return true;
+    }
 }
 
 let list = new SinglyLinkedList();
@@ -126,5 +137,6 @@ console.log(list);
 list.unshift("friend");
 list.unshift("friend2");
 list.unshift("friend3");
+list.set("replaced", 1);
+list.set("replaced", 6);
 console.log(list);
-console.log(`list.get(1)`, list.get(1));
